18e147915b8411941779b031d85515888177ee89
Alessandro Rossi <arossi@siapcn.it>
2023-08-03 09:09:22 +0200

    added css styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First post</title>
+      </Head>
       <h1>First post</h1>
+      <Image
+        src="/images/profile.jpg"
+        height={144}
+        width={144}
+        alt="Alessandro"
+      />
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
